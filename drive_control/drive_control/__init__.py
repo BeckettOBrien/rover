758eada3dd6fa3cb7787cb84dc6c02291ae94dfc
Beckett O'Brien <44044350+BeckettOBrien@users.noreply.github.com>
2026-04-10 17:18:53 -0400

    Remove old folders and packages

--- drive_control/drive_control/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Drive control package for URC rover."""
-
-__version__ = '1.0.0'
-
--- navigation/gps_waypoint_navigator/gps_waypoint_navigator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-gps_waypoint_navigator package.
-
-Provides nodes and utilities for navigating to GPS waypoints using Nav2.
-"""
-
-
